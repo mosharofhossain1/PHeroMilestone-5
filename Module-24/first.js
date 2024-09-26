46a464c11b29a88367fbd160a02eced5d9a7d7d5
Mosharof <md5470415@gmail.com>
2024-09-26 21:19:45 +0600

    DOm traversing element practice

--- Module-24/first.js
+++ Module-24/first.js
@@ -1,2 +1,17 @@
 // console.log('js file is added')
-console.log(document);
+// console.log(document);
+
+// Dom Traversing Elements List 
+/**
+ * 1. getElementsByTagName(); -> Returan HTMLCollection - for of loop kora jai
+ * 
+ * 2. getElementsByClassName(); -> Returan HTMLCollection - for of loop kora jai 
+ * 
+ * 3. getElementById(); ->  Direct element deya dey [jodi kuno element na take tahole null return kore] - kuno loop kora jai na 
+ * 
+ * 4.querySelectorAll() -> NodeList Return kore - for of loop kora jai [forEach Method us ekora jai ]
+ * 
+ * 5. querySelector() -> NodeList return kore [element er first element select kora hoy] - [for of loop use kora jai na] []
+
+ * * */
+
